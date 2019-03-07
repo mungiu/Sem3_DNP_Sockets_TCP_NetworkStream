858aa0b44d594766f84724e9b5dd858d4b2deda3
Andrei Mungiu <andrei_mungiu@yahoo.com>
2019-03-07 15:58:35 +0100

    WIP

--- ServerConnectLib/Class1.cs
+++ ServerConnectLib/Class1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,20 @@ namespace ServerConnectLib
 {
     public class Class1
     {
+        // getting the IP address
+        public IPAddress getIpOfDomain(string domainName)
+        {
+            return new IPAddress(long.Parse(Dns.GetHostAddresses("google.com").ToString()));
+        }
+
+        public async void startTCPListen(IPAddress localIP, int port)
+        {
+            /// NOTE: using System.Net.Sockets;
+            TcpListener tcpListener = new TcpListener(localIP, port);
+            tcpListener.Start();
+            Console.WriteLine("TCP listen started...");
+
+            TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+        }
     }
 }
---
+++ TestServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerConnectLib.Class1 serverLib = new ServerConnectLib.Class1;
+
+            serverLib.test();
+
+        }
+    }
+}
